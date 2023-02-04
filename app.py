587e6959d277fe1d45720d9ba81fce42b438ce0d
John Black <jhblack25@yahoo.com>
2023-02-04 11:00:30 -0600

    add

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+st.header('BlackRock Market Volatility')
+
+data = dict(
+    character=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+    parent=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+    value=[10, 14, 12, 10, 2, 6, 6, 4, 4])
+
+fig = px.sunburst(
+    data,
+    names='character',
+    parents='parent',
+    values='value',
+)
+
+st.plotly_chart(fig, theme=None, use_container_width=True)
