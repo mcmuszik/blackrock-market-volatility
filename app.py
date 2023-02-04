08c6562ada3f88b8384bb633ef1acc8456eee152
John Black <jhblack25@yahoo.com>
2023-02-04 11:36:51 -0600

    added a subheader

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.express as px
 
 st.header('BlackRock Market Volatility')
+st.subheader('created by Marc Muszik and John Black')
 
 data = dict(
     character=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
