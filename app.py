e9e678314e9972c332c3ba7dbe2ef56356897cbb
mcmuszik <marcmuszik@gmail.com>
2023-02-04 18:25:50 -0500

    Added list of for stock ticker dropdown

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ from data import collect_data, list_us_stocks, get_possible_tickers
 from visuals import create_treemap
 from firestore_helpers import User, Transaction, CREDENTIALS
 
-USER_ID = ID = '6efcb234-fcb5-45fb-90e2-6136f46a86b4'
+USER_ID = '6efcb234-fcb5-45fb-90e2-6136f46a86b4'
 
 def ord(n):
-    #source: https://stackoverflow.com/a/16671271/17774866
     # Used for date formatting
+    # source: https://stackoverflow.com/a/16671271/17774866
     return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
 
 today = dt.date.today().strftime('%A, %b. ') + ord(int(dt.date.today().strftime('%-d')))
