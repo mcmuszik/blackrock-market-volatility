b34edb4fba15d3897a26987d5e2b10cacb2bb4d5
mcmuszik <marcmuszik@gmail.com>
2023-02-04 17:25:42 -0500

    Added function to get possible stock tickers for dropdowns

--- app.py
+++ app.py
@@ -2,17 +2,30 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import datetime as date
+import datetime as dt
 
-from data import collect_data, list_us_stocks
+from data import collect_data, list_us_stocks, get_possible_tickers
 from visuals import create_treemap
+from firestore_helpers import User, Transaction, CREDENTIALS
 
-today = date.date.today().strftime('%m/%d/%Y')
+USER_ID = ID = '6efcb234-fcb5-45fb-90e2-6136f46a86b4'
 
-st.header(f'Market Volatility {today}')
+def ord(n):
+    #source: https://stackoverflow.com/a/16671271/17774866
+    # Used for date formatting
+    return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
+
+today = dt.date.today().strftime('%A, %b. ') + ord(int(dt.date.today().strftime('%-d')))
+
+user = User(USER_ID)
+
+
+st.header(f'Market Volatility for {today}.')
 st.subheader('created by Marc Muszik and John Black')
 
-stock_list = list_us_stocks(n=500)
+stock_list = get_possible_tickers()
+
+
 #Use the session state to persist data between page refreshes
 if 'historical_data' not in st.session_state:
     st.session_state['historical_data'], st.session_state['company_data'] = collect_data(stock_list)
