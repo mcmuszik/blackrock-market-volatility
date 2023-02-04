d5db2161a1f446f81bf0e8e1e1fbfd0d57e5d591
mcmuszik <marcmuszik@gmail.com>
2023-02-04 11:08:55 -0500

    Added data.py and main.py
    Co-authored-by: John Black <JHBlack@users.noreply.github.com>

---
+++ data.py
@@ -0,0 +1,59 @@
+import yahoo_fin.stock_info as si
+import yfinance as yf
+import investpy as inv
+import pandas as pd
+import datetime as dt
+import pprint
+from typing import Tuple, List
+from multiprocessing.dummy import Pool
+
+
+def get_historical_and_company_data(ticker:str, **kwargs) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Given a company stock ticker, return two dataframes: 
+    one with historical price data, and one with company information
+    """
+    yf_ticker = yf.Ticker(ticker)
+    historical_data = yf_ticker.history(**kwargs)
+    historical_data['ticker'] = ticker
+    company_data = pd.Series(yf_ticker.info)
+    print(f'Done with {ticker}')
+    return historical_data, company_data
+
+
+def list_us_stocks(n=1000):
+    """
+    Return a list of US stock tickers
+    """
+    us_stocks = inv.get_stocks().query('country == "united states"')
+    ticker_list = us_stocks.head(100)['symbol']
+    return ticker_list
+
+
+def calculate_moving_average(
+    data: pd.DataFrame, 
+    days: int, 
+    partition_by: str='ticker', 
+    over='Close_Diff') -> pd.Series:
+    """
+    Calculate a moving average 
+    """
+    return data.groupby(partition_by)[over].transform(lambda x: x.rolling(days).mean())
+
+
+
+def collect_data(ticker_list: List[str], n_threads=20, start: str = '2020-01-01', end: str = '2022-12-31'):
+    with Pool(20) as p:
+        data = p.map(
+            lambda x: get_historical_and_company_data(x, start=start, end=end), 
+            ticker_list
+            )
+
+    historical_data = pd.concat([i[0] for i in data])
+    company_data = pd.DataFrame([i[1] for i in data])
+    historical_data['Close_Diff'] = historical_data['Close'].diff()
+
+    historical_data['90_day_ma'] = calculate_moving_average(historical_data, days=90)
+    historical_data['5_day_ma'] = calculate_moving_average(historical_data, days=5)
+    return historical_data, company_data
+
