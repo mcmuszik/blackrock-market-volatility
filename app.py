a3e0aeb9bdc7108bff35c0ac9bc3a136cebec6eb
mcmuszik <marcmuszik@gmail.com>
2023-02-05 00:27:38 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -4,29 +4,45 @@ import numpy as np
 import plotly.express as px
 import streamlit.components.v1 as components
 import datetime as dt
-# from data import collect_data, list_us_stocks, get_possible_tickers
-# from visuals import create_treemap
-# from firestore_helpers import User, Transaction, CREDENTIALS
+from data import get_possible_tickers, get_company_data, get_price_data
+from visuals import create_treemap
+from firestore_helpers import User, Transaction
 
-# USER_ID = '6efcb234-fcb5-45fb-90e2-6136f46a86b4'
+#Assume a certain logged-in user
+USER_ID = 'bbbf58ba-823f-4a8c-8007-f22960e83448'
+user = User(USER_ID)
 
-today = date.date.today().strftime('%m/%d/%Y')
+def on_button_click(ticker, volume, date) -> None:
+    new_transaction = [Transaction(ticker, volume, is_purchase=True, datetime=date)]
+    user.update_transactions(new_transaction)
+
+
+def ord(n):
+    # Used for date formatting
+    # source: https://stackoverflow.com/a/16671271/17774866
+    return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
+
+today = dt.date.today().strftime('%A, %b. ') + ord(int(dt.date.today().strftime('%-d')))
+
 
 tab1, tab2 = st.tabs(["Individual Performance", "Market Volatility"])
-today = dt.date.today().strftime('%m/%d/%Y')
 
 with tab1:
     with st.container():
-        st.header(f'Market Volatility Portfolio {today} ' + '📈')
-        st.subheader('created by Marc Muszik and John Black')
+        st.header(f'Portfolio Performance for {today} ' + '📈')
+        st.write('created by Marc Muszik and John Black')
 
+        portfolio = user.portfolio_value()
+        portfolio_plot = px.line(portfolio, x='Date', y='value')
+        st.subheader('Portfolio Performance Over Time')
+        st.plotly_chart(portfolio_plot, theme=None, use_container_width=True)
+    
     # from data import collect_data, list_us_stocks
     # from visuals import create_treemap
 
-    df = pd.DataFrame(
-    np.random.randn(50, 5),
-    columns=('col %d' % i for i in range(5)))
+    df = user.get_transactions_df()
 
+    st.subheader('Portfolio Makeup')
     st.dataframe(df, height=350, width=1000)
 
     def make_or_break(value=0):
@@ -36,52 +52,36 @@ with tab1:
             print("🤑")
 
     with st.container():
-        st.subheader('See your transaction history 👇')
+        st.subheader('Add a new transaction 👇')
         col1, col2 , col3, col4= st.columns(4)
 
     with st.container():
             with col1:
-                option = st.selectbox(
+                ticker = st.selectbox(
                 'Stock Ticker',
-                ('DG', 'AMZN', 'APPL'))
+                tuple(get_possible_tickers()))
 
             with col2:
-                number = st.number_input('Volume', max_value=100000, min_value=0, step=1)
+                volume = st.number_input('Volume', max_value=100000, min_value=0, step=1)
 
             with col3:
-                st.date_input(label="Date")
+                date = st.date_input(label="Date")
 
             with col4:
                 st.text('')
                 st.text('')
-                st.button(label="save transaction")
+                st.button(label="save transaction", on_click=on_button_click)
 
-            st.write('You selected:', option)
     with tab2:
         
         st.header("Market Volatility Chart")
         st.write("This is where the market volatility chart is")
-    # def ord(n):
-    #     # Used for date formatting
-    #     # source: https://stackoverflow.com/a/16671271/17774866
-    #     return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
 
-    # today = dt.date.today().strftime('%A, %b. ') + ord(int(dt.date.today().strftime('%-d')))
+        fig = create_treemap()
 
-    # stock_list = get_possible_tickers()
-    # stock_list = list_us_stocks(n=500)
-    # #Use the session state to persist data between page refreshes
-    # if 'historical_data' not in st.session_state:
-    #     st.session_state['historical_data'], st.session_state['company_data'] = collect_data(stock_list)
+        st.plotly_chart(fig, theme=None, use_container_width=True)
 
-    # fig = create_treemap(
-    #     st.session_state['historical_data'], 
-    #     st.session_state['company_data'] 
-    #     )
-
-    # st.plotly_chart(fig, theme=None, use_container_width=True)
-
-        st.caption('This plot charts the current volatility of individual shares from 500 publicly traded companies compared to their historical volatility. Data is sourced from Yahoo Finance.')
+    st.caption('This plot charts the current volatility of individual shares from 500 publicly traded companies compared to their historical volatility. Data is sourced from Yahoo Finance.')
 
     make_or_break(value=-5)
     make_or_break(value=5)
