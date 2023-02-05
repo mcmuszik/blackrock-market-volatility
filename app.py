dcc686b10e69c9c3582b1b9c4d080484ecbcbad3
mcmuszik <marcmuszik@gmail.com>
2023-02-04 19:54:04 -0500

    Added options for stock tickers

--- app.py
+++ app.py
@@ -2,40 +2,60 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import datetime as dt
+import datetime as date
+import streamlit.components.v1 as components
 
-from data import collect_data, list_us_stocks, get_possible_tickers
-from visuals import create_treemap
-from firestore_helpers import User, Transaction, CREDENTIALS
+from data import get_possible_tickers
 
-USER_ID = '1b57fe1d-5fd5-4a13-b8e3-82bfbb1e8c42'
+today = date.date.today().strftime('%m/%d/%Y')
 
-def ord(n):
-    # Used for date formatting
-    # source: https://stackoverflow.com/a/16671271/17774866
-    return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
+with st.container():
+    st.header(f'Market Volatility {today} ' + '📈')
+    st.subheader('created by Marc Muszik and John Black')
 
-today = dt.date.today().strftime('%A, %b. ') + ord(int(dt.date.today().strftime('%-d')))
-
-user = User(USER_ID)
+# from data import collect_data, list_us_stocks
+# from visuals import create_treemap
 
 
-st.header(f'Market Volatility for {today}.')
-st.subheader('created by Marc Muszik and John Black')
+df = pd.DataFrame(
+   np.random.randn(50, 5),
+   columns=('col %d' % i for i in range(5)))
 
-stock_list = get_possible_tickers()
+st.dataframe(df, height=350, width=1000)
 
 
-#Use the session state to persist data between page refreshes
-if 'historical_data' not in st.session_state:
-    st.session_state['historical_data'], st.session_state['company_data'] = collect_data(stock_list)
+st.subheader('See your transaction history 👇')
+col1, col2 , col3, col4= st.columns(4)
+with st.container():
+    with col1:
+        option = st.selectbox(
+        'Stock Ticker',
+        tuple(get_possible_tickers()))
 
-fig = create_treemap(
-    st.session_state['historical_data'], 
-    st.session_state['company_data'] 
-    )
+    with col2:
+        number = st.number_input('Volume', max_value=100000, min_value=0, step=1)
 
-st.plotly_chart(fig, theme=None, use_container_width=True)
+    with col3:
+        st.date_input(label="Date")
+
+    with col4:
+        st.text('')
+        st.text('')
+        st.button(label="save transaction")
+
+    st.write('You selected:', option)
+
+# stock_list = list_us_stocks(n=500)
+# #Use the session state to persist data between page refreshes
+# if 'historical_data' not in st.session_state:
+#     st.session_state['historical_data'], st.session_state['company_data'] = collect_data(stock_list)
+
+# fig = create_treemap(
+#     st.session_state['historical_data'], 
+#     st.session_state['company_data'] 
+#     )
+
+# st.plotly_chart(fig, theme=None, use_container_width=True)
 
 st.caption('This plot charts the current volatility of individual shares from 500 publicly traded companies compared to their historical volatility. Data is sourced from Yahoo Finance.')
 
